1d040a6d0bb0da8f98163f33dd604c2e2213f858
Jonatan Tadeo <tadeo.calles@outlook.com>
2021-03-04 20:04:56 -0500

    added redux functionaliy and routes now works on the react components

--- leadmanager/frontend/src/components/layout/Header.js
+++ leadmanager/frontend/src/components/layout/Header.js
@@ -4,7 +4,7 @@ export class Header extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-sm navbar-light bg-light">
-                <a className="navbar-brand" href="#">Lead Managerx</a>
+                <a className="navbar-brand" href="#">Lead Manager</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
